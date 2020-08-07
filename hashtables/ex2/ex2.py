577ace4d5ef5b5a6d1a8575e0aa70c93c22ee590
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-08-07 10:36:26 -0700

    working on flight tickets

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,38 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    route = []
+    end = {}
+    for t in tickets:
+        end[t.destination] = t.source
+    for t in tickets:
+        
+    print(end)
+        
     # Your code here
-
+    # the function recieves an array of Tickets with sources and destinations
+    # check each destination to match with a particular source
+    # return the route in the correct order
     return route
+
+# ticket_1 = Ticket("NONE", "PDX")
+# ticket_2 = Ticket("PDX", "DCA")
+# ticket_3 = Ticket("DCA", "NONE")
+
+# tickets = [ticket_1, ticket_2, ticket_3]
+# reconstruct_trip(tickets, 3)
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+reconstruct_trip(tickets, 10)
